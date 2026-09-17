91620652cf2b798669ea9e4218c6a09b1ad23486
danil <d_davletov@internet.ru>
2026-09-17 19:31:04 +0300

    Добавлена настройка кэша для QueryClient

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,7 +6,12 @@ import { ReactNode, useState } from "react"
 
 const client = new QueryClient({
     defaultOptions: {
-        queries: { retry: 1, refetchOnWindowFocus: false },
+        queries: {
+            retry: 1,
+            staleTime: 5 * 60 * 1000,
+            gcTime: 30 * 60 * 1000,
+            refetchOnWindowFocus: false,
+        },
     }
 })
 
